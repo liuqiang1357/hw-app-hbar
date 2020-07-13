15ebb6a8f694ca93e5f07b720332ee9675d540db
liuqiang <liuqiang1357@163.com>
2020-07-13 19:04:57 +0800

    refine example

--- example/test.js
+++ example/test.js
@@ -1,18 +1,23 @@
 import Transport from '@ledgerhq/hw-transport-node-hid';
 import App from '@ont-dev/hw-app-hbar';
+import { Ed25519PublicKey, Transaction } from '@hashgraph/sdk';
 
 async function test() {
   const path = "44'/3030'/1'/0'/0'";
-  const transaction = '0a140a0c0894dab0f8051080d4e69f0312041896f0051202180318c0843d22020878320120721a0a180a0b0a041896f00510ff83af5f0a090a021801108084af5f';
+  const transactionHex = '1a0022410a130a0b08aac3baf80510e0a2ff2a1204189a8506120218041880c2d72f220308f001721c0a1a0a0b0a04189a850610ff83af5f0a0b0a0418cabe05108084af5f';
+  const transaction = Transaction.fromBytes(Uint8Array.from(Buffer.from(transactionHex, 'hex')))
 
   const transport = await Transport.create();
   const app = new App(transport);
 
-  const publicKey = await app.getPublicKey(path);
-  console.log(publicKey);
+  const publicKeyHex = await app.getPublicKey(path);
+  const publicKey = Ed25519PublicKey.fromString(publicKeyHex);
+  console.log(publicKeyHex);
 
-  const signature = await app.signTransaction(path, transaction);
-  console.log(signature);
+  transaction.signWith(publicKey, async array => {
+    return app.signTransaction(path, Buffer.from(array).toString('hex'))
+  });
+  console.log(Buffer.from(transaction.toBytes()).toString('hex'));
 }
 
 test().catch(console.error);
